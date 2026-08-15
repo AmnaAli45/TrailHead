bb0dc632280c938929bfc9be9c7f6b4c84126f13
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-08-15 20:56:29 +0500

    URLS created

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+# Ye file 'store/urls.py' banegi
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("shop/", views.shop, name="shop"),
+    path("product/<int:product_id>/", views.product_detail, name="product_detail"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("cart/", views.cart, name="cart"),
+]
